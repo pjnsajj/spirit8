4cefd8ffdacc551b43dd7f77868ee4e5eee2048e
pjnsajj <nazirbekovjusup@gmail.com>
2020-01-27 19:13:50 +0600

    fixed-initial

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('.mobile-btn').on('click', function () {
+    $('.mobile-btn').toggleClass('show');
+    $('.menu').toggleClass('show-menu');
+});
+
+$('.menu-list-item a').on('click', function () {
+   $('.menu').removeClass('show-menu');
+   $('.mobile-btn').removeClass('show');
+});
+
+
+
